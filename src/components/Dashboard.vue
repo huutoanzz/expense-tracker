9d57c7aac2c77df0d9075d9f34ee99ae26f5ce02
Hữu Toàn <163145972+huutoanzz@users.noreply.github.com>
2026-01-22 15:14:57 +0700

    first-commit

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,107 @@
+  <script setup lang="ts">
+    import { computed, ref } from 'vue'
+
+    import logoVue from '../assets/vue.svg'
+    import logoVite from '../assets/vite.svg'
+    import { ShoppingCart } from '@element-plus/icons-vue'
+    import ProductDisplay from './ProductDisplay.vue'
+    
+    const logoImage1 = ref(logoVue)
+    const logoImage2 = ref(logoVite)
+    const premium = ref(false)
+    const cart = ref([])
+
+    const policies = ref([
+      {
+        id: 'PL001',
+        title: 'Bảo hành',
+        description: '12 tháng chính hãng'
+      },
+      {
+        id: 'PL002',
+        title: 'Đổi trả',
+        description: '7 ngày miễn phí nếu lỗi nhà sản xuất'
+      },
+      {
+        id: 'PL003',
+        title: 'Vận chuyển',
+        description: 'Giao hàng toàn quốc 2–5 ngày'
+      },
+      {
+        id: 'PL004',
+        title: 'Thanh toán',
+        description: 'COD / Chuyển khoản / Ví điện tử'
+      }
+    ])
+
+    const updateCart = ((id) => {
+      cart.value.push(id)
+    })
+
+
+  </script>
+
+  <template>
+    <div class="w-full bg-green-300 flex items-center justify-center gap-4 p-4">
+      <img
+        :src="logoImage1"
+        alt="Logo"
+        class="w-16 h-16 object-contain"
+      />
+      <div class="text-center">
+        <h1 class="text-3xl font-bold text-blue-600">Hello World</h1>
+        <h1 class="text-3xl font-bold text-blue-900">This is my first Vue3 project</h1>
+      </div>
+      <img
+        :src="logoImage2"
+        alt="Logo"
+        class="w-16 h-16 object-contain"
+      />
+    </div>
+
+    <!-- CART ICON -->
+    <div class="fixed top-24 right-6">
+      <div class="relative bg-white p-3 rounded-full">
+        <ShoppingCart class="w-8 h-8 text-green-600" />
+
+        <span
+          v-if="cart.length>0"
+          class="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full"
+        >
+          {{ cart.length }}
+        </span>
+      </div>
+    </div>
+
+    <ProductDisplay :premium="premium" @add-to-cart="updateCart"></ProductDisplay>
+
+    <div class="w-full">
+      <h3 class="text-xl font-bold text-red-600 text-center mt-10">Chính sách & Thông tin</h3>
+      <h3 class="text-xl font-bold text-blue-600 text-right mr-20">Số dịch vụ cung cấp : {{ policies.length }}</h3>
+      <table class="m-4 p-4 border border-solid border-collapse bg-green-50 w-[60%] mx-auto">
+      <thead>
+        <tr class="bg-green-300">
+          <th class="border p-2">Mã dịch vụ</th>
+          <th class="border p-2">Nội dung</th>
+          <th class="border p-2">Chi tiết</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr
+          v-for="pol in policies"
+          :key="pol.id"
+        >
+          <td class="border p-2">{{ pol.id }}</td>
+          <td class="border p-2">{{ pol.title }}</td>
+          <td class="border p-2">{{ pol.description}}</td>
+        </tr>
+      </tbody>
+      </table>
+    </div>
+    
+
+  </template>
+
+  <style scoped>
+  </style>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,143 @@
+  <script setup>
+    import { computed, ref } from 'vue'
+    import logoVue from '../assets/vue.svg'
+    import logoVite from '../assets/vite.svg'
+
+    const props = defineProps({
+        premium:{
+            type:Boolean,
+            required:true
+        }
+    })
+    const emit = defineEmits(['add-to-cart'])
+    const product = ref("Logo Picture")
+    const brand = ref("Draco")
+    const title = computed(() => {
+      return brand.value + " " + product.value
+    })
+    const image = computed(() => {
+      return variants.value[selectedVariant.value].image
+    })
+    const inStock = computed(() => {
+      return variants.value[selectedVariant.value].quantity
+    })
+    const shipping = computed(() => {
+        if(props.premium){
+            return "Free"
+        }
+        return "2.99 $"
+    })
+    const detail = ref(["50% Cotton","30% Wool","20 % Polyester"])
+
+
+    const addToCart = () => {
+        if (variants.value[selectedVariant.value].quantity > 0) {
+            variants.value[selectedVariant.value].quantity -= 1         
+            emit('add-to-cart', variants.value[selectedVariant.value].id) 
+        }
+    }
+
+    const selectedVariant = ref(0)
+    const variants = ref([
+      {id:2234, color:"green", image:logoVue, quantity:12},
+      {id:2235, color:"purple",image:logoVite, quantity:3}
+    ])
+    const updateVariant = (index) => {
+      selectedVariant.value = index
+    }
+  </script>
+
+  <template>
+    <div class="w-full bg-blue-300 mt-10 p-6">
+      <div class="flex flex-col md:flex-row gap-8 items-start justify-center">
+
+        <!-- LEFT: IMAGE -->
+        <div class="flex flex-col items-center md:w-1/3">
+          <img
+            :src="image"
+            alt="Logo"
+            class="w-40 h-40 object-contain"
+          />
+
+          <!-- COLOR VARIANTS (HORIZONTAL) -->
+          <div class="flex gap-3 mt-4">
+            <div
+              v-for="(v , index) in variants"
+              :key="v.id"
+              @click="updateVariant(index)"
+              class="color-circle cursor-pointer"
+              :style="{ backgroundColor: v.color }"
+            ></div>
+          </div>
+        </div>
+
+        <!-- RIGHT: INFO -->
+        <div class="flex flex-col gap-3 md:w-2/3">
+
+          <h1 class="text-3xl font-bold text-red-600">
+            {{ title }}
+          </h1>
+
+          <h1
+            v-show="inStock > 0"
+            class="text-lg font-bold text-green-700"
+          >
+            Item Available !!
+          </h1>
+
+          <h1
+            v-if="inStock > 10"
+            class="text-xl font-bold text-blue-600"
+          >
+            In Stock ({{ inStock }})
+          </h1>
+
+          <h1
+            v-else-if="inStock > 0"
+            class="text-xl font-bold text-yellow-600"
+          >
+            Almost outstock ({{ inStock }})
+          </h1>
+
+          <h1
+            v-else
+            class="text-xl font-bold text-red-600"
+          >
+            Outstock !!
+          </h1>
+
+          <h1
+            class="text-xl font-bold text-black"
+          >
+            Shipping: <span class="text-red-600 text-xl">{{ shipping }}</span> 
+          </h1>
+
+          <!-- DETAIL -->
+          <ul class="list-disc pl-5">
+            <li
+              v-for="d in detail"
+              :key="d"
+              class="text-lg font-medium"
+            >
+              {{ d }}
+            </li>
+          </ul>
+
+          <!-- BUTTON -->
+          <button
+            @click="addToCart"
+            class="bg-orange-500 hover:bg-orange-600 text-white font-bold px-6 py-3 rounded-lg shadow w-fit"
+            :disabled="!inStock"
+            :class="{ disableButton: !inStock }"
+          >
+            ➕ Add to Cart
+          </button>
+
+        </div>
+      </div>
+    </div>
+  </template>
+
+  <style scoped>
+
+  </style>
